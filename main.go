8fde4d3f65f37c7bcba8110e94fc19c8998053d5
coder3457@gmail.com <tar-to-key>
2022-03-27 05:37:01 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// ルーターを準備 --- (*1)
+	router := gin.Default()
+
+	// 自動的にファイルを返すよう設定 --- (*1)
+	router.StaticFS("/static", http.Dir("static"))
+
+	// ルートなら /static/index.html にリダイレクト --- (*2)
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.Redirect(302, "/static/index.html")
+	})
+
+	// フォームの内容を受け取って挨拶する --- (*3)
+	router.GET("/hello", func(ctx *gin.Context) {
+		name := ctx.Query("name")
+		ctx.Header("Content-Type", "text/html; charset=UTF-8")
+		ctx.String(200, "<h1>Hello, "+name+"</h1>")
+	})
+
+	// サーバーを起動 --- (*3)
+	err := router.Run("127.0.0.1:8888")
+	if err != nil {
+		log.Fatal("サーバー起動に失敗", err)
+	}
+}
